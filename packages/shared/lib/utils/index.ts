9c635bef7c25876adb02edb855151e806ada5d54
wavesbig <34082148+wavesbig@users.noreply.github.com>
2025-09-04 10:41:30 +0800

    fix: 统一到飞书

--- packages/shared/lib/utils/index.ts
+++ packages/shared/lib/utils/index.ts
@@ -2,5 +2,4 @@ export * from './helpers.js';
 export * from './colorful-logger.js';
 export * from './init-app-with-shadow.js';
 export * from './feishu.js';
-export * from './config-validation.js';
 export type * from './types.js';
